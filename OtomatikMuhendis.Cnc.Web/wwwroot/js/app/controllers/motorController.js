8dee697030970f9722d8ddca3206cc31fafc2e68
Olcay Bayram <olcaybayram@gmail.com>
2019-08-30 01:52:33 +0200

    Playground for positioning, run logs

--- OtomatikMuhendis.Cnc.Web/wwwroot/js/app/controllers/motorController.js
+++ OtomatikMuhendis.Cnc.Web/wwwroot/js/app/controllers/motorController.js
@@ -1,4 +1,6 @@
 ﻿var MotorController = function (leapService, motorService) {
+    var logStep = 0, runLogInterval;
+
     var init = function (container) {
         $("#btnPenUp").click(penUp);
         $("#btnPenDown").click(penDown);
@@ -8,7 +10,49 @@
         $(".btnGoTo").click(goToFixed);
         $("#btnToggleLeap").click(enableLeap);
 
+        $("#btnRunLog").click(runLog);
+
+        $("#divPlayground").click(getPositionFromPlayground);
+
         leapService.init(gotPosition);
+    };
+
+    var getPositionFromPlayground = function(e) {
+        var offset = $(this).offset();
+        var relativeX = (e.pageX - offset.left);
+        var relativeY = (e.pageY - offset.top);
+
+        $('#txtX').val(Math.round(relativeY) * 2 - 600);
+        $('#txtY').val(Math.round(relativeX) * 2 - 436);
+      
+        goTo();
+    };
+
+    var runLog = function() {
+        $("#btnRunLog").removeClass("btn-success").addClass("btn-danger").html("Stop").unbind().click(stopRunningLog);
+        logStep = 0;
+        runLogInterval = setInterval(function(){ 
+
+            var splitted = $('#txtLog').val().split("\n"); 
+            var position = JSON.parse(splitted[logStep]);
+
+            $('#txtX').val(position[0]);
+            $('#txtY').val(position[1]);
+
+            goTo();
+            logStep++;
+
+            if(splitted.length <= logStep + 1){
+                stopRunningLog();
+            }
+
+         }, 5000);
+    };
+
+    var stopRunningLog = function() {
+        $("#btnRunLog").removeClass("btn-danger").addClass("btn-success").html("Run Log").unbind().click(runLog);
+
+        clearInterval(runLogInterval);
     };
 
     var enableLeap = function (e) {
@@ -26,6 +70,11 @@
     };
 
     var penDown = function (e) {
+        var x = $('#txtX').val();
+        var y = $('#txtY').val();
+
+        $("#txtLog").val($("#txtLog").val() + "[" + x + "," + y + "]\n");
+
         motorService.penDown(done, fail);
     };
 
@@ -56,6 +105,8 @@
     var goTo = function (e) {
         var x = $('#txtX').val();
         var y = $('#txtY').val();
+
+        $("#spnPencil").css({top: ((Number(x) + 600) / 2) + "px", left: ((Number(y) + 436) / 2) + "px"});
 
         motorService.goTo(x, y, done, fail);
     };
